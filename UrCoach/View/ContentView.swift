5f9bb466c9a4773951a85a8eccd66903dfa1997b
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-08-25 13:29:23 -0700

    Add skeleton iOS app.

---
+++ UrCoach/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UrCoach
+//
+//  Created by Stephen Bensley on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UrCoach/View/UrCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrCoachApp.swift
+//  UrCoach
+//
+//  Created by Stephen Bensley on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct UrCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
